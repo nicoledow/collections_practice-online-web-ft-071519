79339e23a323a45a9a00f8148c0b5b0272445f11
Nicole Dow <nikki.dow71@gmail.com>
2019-07-15 22:42:43 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,53 @@
+def sort_array_asc(array)
+  sorted_array = array.sort
+  sorted_array
+end
 
+def sort_array_desc(array)
+  sorted_array = array.sort.reverse
+  sorted_array
+end
+
+def sort_array_char_count(array)
+  sorted_array = array.sort {|x, y| x.length <=> y.length}
+  sorted_array
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  reversed_array = array.reverse
+  reversed_array
+end
+
+def kesha_maker(strings)
+  keshafied = []
+  
+  strings.each do |string|
+    string[2] = "$"
+    keshafied << string
+  end
+  
+  keshafied
+end
+
+def find_a(array)
+  array.select {|word| word.start_with?("a") || word.start_with?("A")}
+end
+
+def sum_array(array)
+  array.reduce {|sum, n| sum + n}
+end
+
+def add_s(array)
+  array.each_with_index.map do |word, index|
+    if index == 1
+      word = word
+    else
+      word = "#{word}s"
+    end
+  end
+end
